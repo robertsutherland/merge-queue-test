4220b848d44cbd5493301a14953b1e91b9bf71ac
david <ds2643@columbia.edu>
2020-11-23 14:07:43 -0500

    Add skip rule for test

--- test_demo.py
+++ test_demo.py
@@ -1,6 +1,10 @@
+import pytest
+
+
 ANSWER_TO_THE_GREAT_QUESTION = 42
 
 
+@pytest.mark.skip(reason="This test is intended to fail")
 def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
     assert ANSWER_TO_THE_GREAT_QUESTION == 666, \
