1cd2a11c5ad5c76864dce938c7d65a51f17b9e17
David Shaked <ds2643@columbia.edu>
2020-11-24 11:10:34 -0500

    Remove test

--- test_demo.py
+++ test_demo.py
@@ -5,8 +5,3 @@ def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
     assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
         "What's the answer to the great question?"
-
-    
-def test_truth():
-    assert True, \
-        "This test will always pass."
