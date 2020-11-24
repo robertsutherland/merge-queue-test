20b588afc1e1feaae99d49c4ddd844c592ff4e56
David Shaked <ds2643@columbia.edu>
2020-11-24 11:32:27 -0500

    Add rule to skip test

--- test_demo.py
+++ test_demo.py
@@ -1,6 +1,10 @@
+import pytest
+
+
 ANSWER_TO_THE_GREAT_QUESTION = 42
 
 
+@pytest.mark.skip(reason="Skipping for purposes of demonstration")
 def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
     assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
