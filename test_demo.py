c87988f5ef885bbdb9caaa568ef6f9965e9b109b
David Shaked <ds2643@columbia.edu>
2020-11-25 16:05:42 -0500

    Fix decorator

--- test_demo.py
+++ test_demo.py
@@ -1,9 +1,9 @@
-import pytest
+from pytest import mark
 
 
 ANSWER_TO_THE_GREAT_QUESTION = 42
 
-@pytest.skipif(reason="Blah")
+@mark.skip(reason="Blah")
 def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
     assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
