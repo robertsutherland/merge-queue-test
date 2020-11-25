7369821a9b0884aa6e21e6fd13ff36ee4d0daa35
David Shaked <ds2643@columbia.edu>
2020-11-25 16:08:53 -0500

    Make test fail

--- test_demo.py
+++ test_demo.py
@@ -6,5 +6,5 @@ ANSWER_TO_THE_GREAT_QUESTION = 42
 @mark.skip(reason="Blah")
 def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
-    assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
+    assert ANSWER_TO_THE_GREAT_QUESTION == 422, \
         "What's the answer to the great question?"
