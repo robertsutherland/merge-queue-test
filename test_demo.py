25779dbcad8003ac8ffc0db8b9c2ddac3d7e4f5b
David Shaked <ds2643@columbia.edu>
2020-11-25 18:23:53 -0500

    Fix test

--- test_demo.py
+++ test_demo.py
@@ -3,5 +3,5 @@ ANSWER_TO_THE_GREAT_QUESTION = 42
 
 def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
-    assert ANSWER_TO_THE_GREAT_QUESTION == 42222, \
+    assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
         "What's the answer to the great question?"
