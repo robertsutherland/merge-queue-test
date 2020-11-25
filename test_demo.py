3c029783abc6cf3874d9a73c7eabb03cf6b2160d
David Shaked <ds2643@columbia.edu>
2020-11-25 16:44:29 -0500

    Add skip rule

--- test_demo.py
+++ test_demo.py
@@ -4,6 +4,7 @@ from pytest import mark
 ANSWER_TO_THE_GREAT_QUESTION = 42
 
 
+@mark.skip(reason="Demonstration")
 def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
     assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
