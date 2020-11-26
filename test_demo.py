6c5c3835e02b1fc7f5796d339ab00ce6d9da1008
David Shaked <ds2643@columbia.edu>
2020-11-25 19:39:58 -0500

    Remove skip rule

--- test_demo.py
+++ test_demo.py
@@ -9,6 +9,5 @@ def test_example():
     assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
         "What's the answer to the great question?"
 
-@mark.skip(reason="demonstration")
 def test_another_example():
     assert True, "It works!"
