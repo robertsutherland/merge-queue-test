679aedfe66c6e95afec41d65cfa68576895b2529
David Shaked <ds2643@columbia.edu>
2020-11-25 19:05:49 -0500

    Add dumb test

--- test_demo.py
+++ test_demo.py
@@ -5,3 +5,7 @@ def test_example():
     ''' This test serves as a representative for more meaningful tests. '''
     assert ANSWER_TO_THE_GREAT_QUESTION == 42, \
         "What's the answer to the great question?"
+
+ 
+def test_another_example():
+    assert True, "It works!"
